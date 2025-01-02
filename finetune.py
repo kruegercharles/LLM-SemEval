33abb767b74c69029667438b7e05c5b1dc3760ef
Charles Krüger <charles.krueger@mailbox.tu-dresden.de>
2025-01-02 13:43:48 +0100

    loading model and checking base model seems to work, next up: loading dataset

--- finetune.py
+++ finetune.py
@@ -1,5 +1,5 @@
 import torch
-from transformers import LlamaForCausalLM, AutoTokenizer, BitsAndBytesConfig
+from transformers import LlamaForCausalLM, AutoTokenizer
 from llama_recipes.configs import train_config as TRAIN_CONFIG
 from llama_recipes.utils.dataset_utils import get_dataloader
 from peft import get_peft_model, prepare_model_for_kbit_training, LoraConfig
@@ -8,36 +8,48 @@ from llama_recipes.configs import lora_config as LORA_CONFIG
 import torch.optim as optim
 from llama_recipes.utils.train_utils import train
 from torch.optim.lr_scheduler import StepLR
+import os
+
+# Config
+MODEL_PATH = "models/checkpoints/Llama3.1-8B-Instruct/"
+OUTPUT_DIR = "meta-llama-semeval"
 
 
 def load_model():
+    # make sure the file exists
+    if not os.path.exists(MODEL_PATH):
+        raise FileNotFoundError(f"Model file {MODEL_PATH} not found")
+
+    # See: https://github.com/meta-llama/llama-recipes/tree/main/recipes/quickstart/finetuning
     train_config = TRAIN_CONFIG()
-    train_config.model_name = "meta-llama/Meta-Llama-3.1-8B"
+    train_config.model_name = MODEL_PATH
     train_config.num_epochs = 1
     train_config.run_validation = False
     train_config.gradient_accumulation_steps = 4
     train_config.batch_size_training = 1
     train_config.lr = 3e-4
-    train_config.use_fast_kernels = True
+    train_config.use_fast_kernels = False
     train_config.use_fp16 = True
     train_config.context_length = (
         1024 if torch.cuda.get_device_properties(0).total_memory < 16e9 else 2048
-    )  # T4 16GB or A10 24GB
+    )
     train_config.batching_strategy = "packing"
-    train_config.output_dir = "meta-llama-semeval"
+    train_config.output_dir = OUTPUT_DIR
     train_config.use_peft = True
 
-    config = BitsAndBytesConfig(
-        load_in_8bit=True,
-    )
+    # FIXME: irgendwie funktioniert es nicht die Quantisierung zu aktivieren
+    #    config = BitsAndBytesConfig(
+    #        load_in_8bit=True,
+    #    )
 
     model = LlamaForCausalLM.from_pretrained(
         train_config.model_name,
         device_map="auto",
-        quantization_config=config,
+        # quantization_config=config,
         use_cache=False,
         attn_implementation="sdpa" if train_config.use_fast_kernels else None,
         torch_dtype=torch.float16,
+        local_files_only=True,
     )
 
     tokenizer = AutoTokenizer.from_pretrained(train_config.model_name)
@@ -46,28 +58,12 @@ def load_model():
 
 
 def check_base_model(model, tokenizer):
-    eval_prompt = """
-    Summarize this dialog:
-    A: Hi Tom, are you busy tomorrow’s afternoon?
-    B: I’m pretty sure I am. What’s up?
-    A: Can you go with me to the animal shelter?.
-    B: What do you want to do?
-    A: I want to get a puppy for my son.
-    B: That will make him so happy.
-    A: Yeah, we’ve discussed it many times. I think he’s ready now.
-    B: That’s good. Raising a dog is a tough issue. Like having a baby ;-)
-    A: I'll get him one of those little dogs.
-    B: One that won't grow up too big;-)
-    A: And eat too much;-))
-    B: Do you know which one he would like?
-    A: Oh, yes, I took him there last Monday. He showed me one that he really liked.
-    B: I bet you had to drag him away.
-    A: He wanted to take it home right away ;-).
-    B: I wonder what he'll name it.
-    A: He said he’d name it after his dead hamster – Lemmy  - he's  a great Motorhead fan :-)))
-    ---
-    Summary:
-    """
+    system_prompt = "There are 5 different categories for emotions: Anger, Fear, Joy, Sadness, Surprise. Classify the following sentence in none or one or more of these categories. Only answer with none or the appropriate category or categories."
+
+    eval_prompt = (
+        system_prompt
+        + " Sentence: We ordered some food at Mcdonalds instead of buying food at the theatre because of the ridiculous prices the theatre has."
+    )
 
     model_input = tokenizer(eval_prompt, return_tensors="pt").to("cuda")
 
@@ -84,10 +80,14 @@ def check_base_model(model, tokenizer):
 
 
 def load_the_preprocessed_dataset(tokenizer, train_config):
-    dataset = ""  # TODO: fill in the dataset path
+    dataset_path = "data/codabench_data/train/eng_a_parsed.json"
 
-    train_dataloader = get_dataloader(tokenizer, dataset, train_config)
-    eval_dataloader = get_dataloader(tokenizer, dataset, train_config, "val")
+    # make sure the file exists
+    if not os.path.exists(dataset_path):
+        raise FileNotFoundError(f"Dataset file {dataset_path} not found")
+
+    train_dataloader = get_dataloader(tokenizer, dataset_path, train_config)
+    eval_dataloader = get_dataloader(tokenizer, dataset_path, train_config, "val")
 
     return train_dataloader, eval_dataloader
 
@@ -150,44 +150,51 @@ def evaluate_model(model, tokenizer, model_input):
         )
 
 
-# TODO:
-# - angucken, wie das Dataset aussieht, was die in dem Tutorial aussieht, um zu gucken, wie unseres aussiehen sollte
-# - Überlegen, wie das Dataset preprocesset werden sollte
-
-
 def main():
     # Step 1: Load the model
     model, tokenizer, train_config = load_model()
-    print("Model loaded")
+    print_checkpoint("Model loaded")
 
     # Step 2: Check the base model
     model_input, model = check_base_model(model, tokenizer)
-    print("Base model checked")
+    print_checkpoint("Base model checked")
 
     # Step 3: Load the preprocessed dataset
     train_dataloader, eval_dataloader = load_the_preprocessed_dataset(
         tokenizer, train_config
     )
-    print("Dataset loaded")
+    print_checkpoint("Dataset loaded")
+
+    exit()
 
     # Step 4: Prepare the model for PEFT
     model = prepare_model_for_PEFT(model)
-    print("Model prepared for PEFT")
+    print_checkpoint("Model prepared for PEFT")
 
     # Step 5: Fine-tune the model
     results = fine_tune_the_model(
         model, tokenizer, train_dataloader, eval_dataloader, train_config
     )
-    print("Model fine-tuned")
+    print("Results of fine-tuning: ", results)
+    print_checkpoint("Model fine-tuned")
 
     # Step 6: Save the model checkpoint
     save_model_checkpoint(model, train_config)
-    print("Model checkpoint saved")
+    print_checkpoint("Model checkpoint saved")
 
     # Step 7: Evaluate the model
     evaluate_model(model, tokenizer, model_input)
-    print("Model evaluated")
+    print_checkpoint("Model evaluated")
 
 
 if __name__ == "__main__":
     main()
+
+
+def print_checkpoint(message: str):
+    # just a helper function to make the output more readable when reaching a checkpoint
+    print(" ")
+    print("=" * 50)
+    print(message)
+    print("=" * 50)
+    print(" ")
