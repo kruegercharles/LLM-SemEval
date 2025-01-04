bc2664cb20443a0bee72af88957316cd8fd1c675
Charles Krüger <charles.krueger.social@gmail.com>
2025-01-04 13:57:18 +0100

    update convertion of llama model and gitignore

--- convert_llama_model.py
+++ convert_llama_model.py
@@ -5,16 +5,17 @@ import os
 When downloading Llama models from the Llama website, the model is saved in a .pth file, but we need a .bin file.
 """
 
-PATH = "models/checkpoints/Llama3.1-8B-Instruct/consolidated.00.pth"
+BASE_PATH = "models/checkpoints/Llama3.2-1B-Instruct-int4-qlora-eo8/"
+FULLPATH = BASE_PATH + "consolidated.00.pth"
 
 # check if the file exists
-if not os.path.exists(PATH):
-    raise FileNotFoundError(f"Model file {PATH} not found")
+if not os.path.exists(FULLPATH):
+    raise FileNotFoundError(f"Model file {FULLPATH} not found")
 
 # Load the .pth file
-state_dict = torch.load(PATH)
+state_dict = torch.load(FULLPATH)
 
 # Save in PyTorch format
-torch.save(state_dict, "pytorch_model.bin")
+torch.save(state_dict, BASE_PATH+ "pytorch_model.bin")
 
 print("Model saved in PyTorch format.")
