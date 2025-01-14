a58fdcaff588fe4a65edb0256a0a5797c1cbb611
Elia Rühle <elia.ruehle@outlook.de>
2025-01-14 18:52:46 +0100

    making `slurm` HPC ready

--- slurm.sh
+++ slurm.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --mem=12G
+#SBATCH --mem=48G
 #SBATCH --gres=gpu:1
-#SBATCH --time=00:10:00
-#SBATCH --output=/data/cat/ws/chkr211d-secretllm/mylogs/log-%j.log
-#SBATCH --error=/data/cat/ws/chkr211d-secretllm/mylogs/errors-%j.log
+#SBATCH --time=24:00:00
+#SBATCH --output=/data/walrus/ws/elru535b-llm_emotion/logs/log-%j.log
+#SBATCH --error=/data/walrus/ws/elru535b-llm_emotion/logs/errors-%j.log
 #SBATCH --mail-type=begin,end,fail
-#SBATCH --mail-user=charles.krueger@mailbox.tu-dresden.de
+#SBATCH --mail-user=elia.ruehle@mailbox.tu-dresden.de
 #SBATCH --account=p_scads_llm_secrets
 
-python3 -m venv --system-site-package /data/cat/ws/chkr211d-secretllm
-source /data/cat/ws/chkr211d-secretllm/bin/activate
+source /data/walrus/ws/elru535b-llm_emotion/bin/activate
 
-torchrun --nproc_per_node 1 example_text_completion.py --ckpt_dir models/checkpoints/Llama3.1-8B-Instruct --tokenizer_path models/checkpoints/Llama3.1-8B-Instruct/tokenizer.model --max_seq_len 128 --max_batch_size 4
+python3 train.py
