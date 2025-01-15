41db939c011fe003ec94c076583274309111910a
Elia Rühle <elia.ruehle@outlook.de>
2025-01-15 16:30:02 +0100

    slurm script updates

--- slurm.sh
+++ slurm.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --mem=48G
+#SBATCH --mem=80G
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
 #SBATCH --gres=gpu:1
 #SBATCH --time=24:00:00
 #SBATCH --output=/data/walrus/ws/elru535b-llm_emotion/logs/log-%j.log
@@ -9,8 +11,9 @@
 #SBATCH --mail-user=elia.ruehle@mailbox.tu-dresden.de
 #SBATCH --account=p_scads_llm_secrets
 
+
 source /data/walrus/ws/elru535b-llm_emotion/bin/activate
 
-python3 train.py
+python3 /data/walrus/ws/elru535b-llm_emotion/LLM-SemEval/src/train.py
 
 deactivate
