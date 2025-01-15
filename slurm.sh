cb2132625015ef2d6eb2dbb32909020b3141bb1c
Elia Rühle <elia.ruehle@outlook.de>
2025-01-15 17:06:55 +0100

    slurm script updates

--- slurm.sh
+++ slurm.sh
@@ -3,11 +3,11 @@
 #SBATCH --partition=capella
 #SBATCH --mem=80G
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=2
 #SBATCH --gres=gpu:1
 #SBATCH --time=24:00:00
-#SBATCH --output=/data/walrus/ws/elru535b-llm_emotion/logs/log-%j.log
-#SBATCH --error=/data/walrus/ws/elru535b-llm_emotion/logs/errors-%j.log
+#SBATCH --output=/data/cat/ws/elru535b-llm_secrets/logs/log-%j.log
+#SBATCH --error=/data/walrus/ws/elru535b-llm_secrets/logs/errors-%j.log
 #SBATCH --mail-type=begin,end,fail
 #SBATCH --mail-user=elia.ruehle@mailbox.tu-dresden.de
 #SBATCH --account=p_scads_llm_secrets
@@ -17,8 +17,8 @@ module load release/24.04
 module load GCCcore/12.3.0
 module load Python/3.11.3
 
-source /data/walrus/ws/elru535b-llm_emotion/bin/activate
+source /data/cats/ws/elru535b-llm_secrets/bin/activate
 
-python3 /data/walrus/ws/elru535b-llm_emotion/LLM-SemEval/src/train.py
+python3 /data/cat/ws/elru535b-llm_secrets/LLM-SemEval/src/train.py
 
 deactivate
