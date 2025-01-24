bb43a5b3a4b3fcef3aa2739324676457ffffc6e9
elru535b <elru535b@c1.capella.hpc.tu-dresden.de>
2025-01-24 16:37:47 +0100

    changed slurm script

--- slurm.sh
+++ slurm.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=48G
 #SBATCH --gres=gpu:1
-#SBATCH --time=30:00:00
+#SBATCH --time=12:00:00
 #SBATCH --output=/data/cat/ws/elru535b-llm_secrets/logs/log-%j.log
 #SBATCH --error=/data/cat/ws/elru535b-llm_secrets/logs/errors-%j.log
 #SBATCH --mail-type=begin,end,fail
@@ -19,4 +19,4 @@ module load Python/3.11.3
 
 source /data/cat/ws/elru535b-llm_secrets/bin/activate
 
-srun python3 /data/cat/ws/elru535b-llm_secrets/LLM-SemEval/src/train.py
+srun python3 /data/cat/ws/elru535b-llm_secrets/LLM-SemEval/src/main.py
