ba62dcaa1c0cbdb124698cdb3c07a0d189fa99d0
Elia Rühle <elia.ruehle@outlook.de>
2025-01-27 21:34:21 +0100

    prepared training script

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ import torch.utils
 from torch.utils.data import Dataset, DataLoader
 from sklearn.model_selection import KFold
 import torch.utils.data
-from transformers import RobertaTokenizer, RobertaForSequenceClassification
+from transformers import RobertaTokenizer
 import torch.nn as nn
 import torch.optim as optim
 from omegaconf import DictConfig
@@ -15,14 +15,11 @@ from misc.misc import search_available_devices, plot_losses, statistics_to_csv, 
 from data.dataset import EmotionData
 from models.lm_classifier import *
 from sklearn.metrics import f1_score
+from torchviz import make_dot
 
 def select_model(name, backbone, num_labels, device):
     if name == 'base':
-        return RobertaForSequenceClassification.from_pretrained(
-        'roberta-base',
-        num_labels = 7,
-        problem_type='multi_label_classification',
-    ).to(device)
+        return RobertaForSequenceClassificationPure(backbone, num_labels).to(device=device)
     elif name == 'deep':
         return RobertaForSequenceClassificationDeep(backbone, num_labels).to(device=device)
     elif name == 'mean':
@@ -35,7 +32,7 @@ def select_model(name, backbone, num_labels, device):
         raise ValueError('Specified model name is not available!')
 
 
-def train(fold, epochs, model, train_loader, val_loader, optimizer, criterion, device):
+def train(fold, epochs, model, train_loader, val_loader, test_loader, optimizer, criterion, device):
 
     train_losses, val_losses, train_acc, val_acc, f1_train, f1_val = list(), list(), list(), list(), list(), list()
 
@@ -48,10 +45,9 @@ def train(fold, epochs, model, train_loader, val_loader, optimizer, criterion, d
         for batch in train_loader:
             input_ids, attention_mask, labels = batch['input_ids'].to(device), batch['attention_mask'].to(device), batch['labels'].to(device)
             optimizer.zero_grad()
-            outputs = model(input_ids=input_ids, attention_mask=attention_mask).logits
+            outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+            print(outputs)
             loss = criterion(outputs, labels)
-            #print(f'Inputs: {labels}')
-            #print(f'Outputs: {outputs}')
             loss.backward()
             optimizer.step()
             f1 = f1_score(labels.detach().cpu().numpy(), (torch.where(torch.sigmoid(outputs) >=0.5, torch.tensor(1.0), torch.tensor(0.0))).detach().cpu().numpy(), average='macro', zero_division=0.0)
@@ -82,7 +78,7 @@ def train(fold, epochs, model, train_loader, val_loader, optimizer, criterion, d
             print(f'Start Validation Epoch {epoch+1}.')
             for batch in val_loader:
                 input_ids, attention_mask, labels = batch['input_ids'].to(device), batch['attention_mask'].to(device), batch['labels'].to(device)
-                outputs = model(input_ids=input_ids, attention_mask=attention_mask).logits
+                outputs = model(input_ids=input_ids, attention_mask=attention_mask)
                 #print(outputs.logits)
                 #print(f'Inputs: {labels}')
                 #print(f'Outputs: {outputs}')
@@ -98,6 +94,16 @@ def train(fold, epochs, model, train_loader, val_loader, optimizer, criterion, d
         val_acc.append(correct_val / len(val_loader))
         f1_val.append(sum(f1_v) / len(f1_v))
         print(f"Epoch {epoch+1}: Train Loss = {train_losses[-1]:.4f}, Validation Loss = {val_losses[-1]:.4f}, Validation Acc. = {val_acc[-1]:.4f}")
+    
+    ### Test Model on SemEval Test Dataset ###
+    model.eval()
+    with torch.no_grad():
+        print(f'Start Testing on Test Dataset!')
+        for batch in test_loader:
+            input_ids, attention_mask, labels = batch['input_ids'].to(device), batch['attention_mask'].to(device), batch['labels'].to(device)
+            outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+            loss = criterion(outputs, labels.float())
+
 
     statistics_to_csv(os.path.join(os.path.dirname(__file__), f'../outputs/statistics/train_loss_fold_{fold}.csv'), train_losses)
     statistics_to_csv(os.path.join(os.path.dirname(__file__), f'../outputs/statistics/validation_loss_fold_{fold}.csv'), val_losses)
@@ -110,59 +116,32 @@ def train(fold, epochs, model, train_loader, val_loader, optimizer, criterion, d
 def cross_validation(cfg: DictConfig):
     #search for available devices
     device = search_available_devices()
-    print(f'*** Found the following device: {device}. Start configuting training.')
-
-    '''
-    #######
-    m = RobertaForSequenceClassification.from_pretrained(
-        'roberta-base',
-        num_labels = 7,
-        problem_type='multi_label_classification',
-    ).to(device)
-
-    m1 = RobertaForSequenceClassification1('roberta-base', 7).to(device)
-    print(m)
-    print('##################################################################################################')
-    print(m1)
-    quit()
-
-    #####
-    '''
-
+    print(f'*** Found the following device: {device}. Start configuting training. ***')
 
     #dataset = EmotionData(os.path.join(os.path.dirname(__file__), cfg.data), cfg.backbone)
     kfold = KFold(n_splits=5, shuffle=True, random_state=42)
 
-    train_dataset = EmotionData(os.path.join(os.path.dirname(__file__), cfg.data), cfg.backbone)
+    dataset = EmotionData(os.path.join(os.path.dirname(__file__), cfg.data), cfg.backbone)
     test_dataset = EmotionData(os.path.join(os.path.dirname(__file__), '../data/eng_a_parsed_test.json'), cfg.backbone)
     
-    '''
+    
     for fold, (train_idx, val_idx) in enumerate(kfold.split(np.arange(len(dataset)))):
         print(f'Starting fold {fold+1}/{5}.')
 
         # create train and validation datasets
         train_set = torch.utils.data.Subset(dataset, train_idx)
-        val_subset = torch.utils.data.Subset(dataset, val_idx)
+        val_set = torch.utils.data.Subset(dataset, val_idx)
 
         # create dataloaders 
         train_loader = DataLoader(train_set, batch_size=cfg.batch_size, shuffle=True)
-        val_loader = DataLoader(val_subset, batch_size=cfg.batch_size)
+        val_loader = DataLoader(val_set, batch_size=cfg.batch_size)
+        test_loader = DataLoader(test_dataset, batch_size=cfg.batch_size)
 
         model = select_model(cfg.model_name, cfg.backbone, cfg.num_labels, device)
-        optimizer = optim.AdamW(model.parameters(), lr=cfg.lr)
+        optimizer = optim.AdamW(model.parameters(), lr=cfg.lr, weight_decay=0.1)
         criterion = nn.BCEWithLogitsLoss().to(device=device)
 
-        train(fold+1, cfg.epochs, model, train_loader, val_loader, optimizer, criterion, device)
-    '''
-    train_loader = DataLoader(train_dataset, batch_size=cfg.batch_size)#, shuffle=True)
-    val_loader = DataLoader(test_dataset, batch_size=cfg.batch_size)
-
-    model = select_model(cfg.model_name, cfg.backbone, cfg.num_labels, device)
-    optimizer = optim.AdamW(model.parameters(), lr=cfg.lr, weight_decay=0.1)
-    criterion = nn.BCEWithLogitsLoss().to(device=device)
-
-    train(1, cfg.epochs, model, train_loader, val_loader, optimizer, criterion, device)
-
+        train(fold+1, cfg.epochs, model, train_loader, val_loader, test_loader, optimizer, criterion, device)
 
 if __name__ == '__main__':
     cross_validation()
